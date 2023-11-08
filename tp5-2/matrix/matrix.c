135d9f55cb2593949ec0eb995a2443dc32e65cf2
qelal <qelal@batareika>
2023-11-08 11:46:22 +0100

    changement de couleurs

--- tp5-2/matrix/matrix.c
+++ tp5-2/matrix/matrix.c
@@ -7,10 +7,12 @@
 
 void matrix(void) {
 
+    int vert[] = {2,92,102};
+
     srand(time(NULL));
     while (true) {
         for(int i=0;i<80;i++) {
-            printf("\x1b[32m%c\x1b[0m",rand()%126+32);
+            printf("\x1b[%dm%c\x1b[0m",vert[rand()%2+0],rand()%126+32);
             usleep(500);
         }
         printf("\n");
